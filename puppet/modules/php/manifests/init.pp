f3dc2ca582a62cb7fa524efe73502c21f8d33dd2
Marius <kisphp@users.noreply.github.com>
2016-02-13 13:26:35 +0100

    included php5-intl extension

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,6 +1,6 @@
 class php {
 
-  package { ['php5-fpm', 'php5-gd', 'php5-mysql', 'php5-cli', 'php-pear', 'php5-mcrypt', 'mcrypt', 'php5-imagick', 'imagemagick',
+  package { ['php5-fpm', 'php5-gd', 'php5-mysql', 'php5-cli', 'php-pear', 'php5-intl', 'php5-mcrypt', 'mcrypt', 'php5-imagick', 'imagemagick',
     'php5-curl', 'php5-memcache', 'php5-memcached', 'php5-sqlite', 'memcached', 'phpunit']:
     ensure => present,
     require => Exec['apt-get-update'],
