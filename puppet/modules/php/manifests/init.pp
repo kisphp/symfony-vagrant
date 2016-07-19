327c6ac7cb86c42712762bed7f3331d456807f3c
Marius Bogdan <Marius@Mariuss-MBP.localdomain>
2016-07-19 14:30:21 +0200

    install php 5.6

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,4 +1,5 @@
 class php {
+  version => '5.6',
 
   package { ['php5-fpm', 'php5-gd', 'php5-mysql', 'php5-cli', 'php-pear', 'php5-intl', 'php5-mcrypt', 'mcrypt', 'php5-imagick', 'imagemagick',
     'php5-curl', 'php5-dev', 'php5-memcache', 'php5-memcached', 'php5-sqlite', 'memcached', 'phpunit']:
