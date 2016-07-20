d8450c711162819d7b1b7d83fafc179d0879076f
kisphp <mariusbogdan83@gmail.com>
2016-07-20 07:32:51 +0200

    updated php version

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,10 +1,20 @@
 class php {
-  version => '5.6',
 
-  package { ['php5-fpm', 'php5-gd', 'php5-mysql', 'php5-cli', 'php-pear', 'php5-intl', 'php5-mcrypt', 'mcrypt', 'php5-imagick', 'imagemagick',
+  exec { 'add-ppa-repo':
+    command => 'sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php',
+    path => '/usr/bin',
+    require => Exec['apt-get-update'],
+  }
+  exec { 'local-apt-update':
+    command => 'sudo apt-get update',
+    path => '/usr/bin',
+    require => Exec['add-ppa-repo'],
+  }
+
+  package { ['php5.6', 'php5-fpm', 'php5-gd', 'php5-mysql', 'php5-cli', 'php-pear', 'php5-intl', 'php5-mcrypt', 'mcrypt', 'php5-imagick', 'imagemagick',
     'php5-curl', 'php5-dev', 'php5-memcache', 'php5-memcached', 'php5-sqlite', 'memcached', 'phpunit']:
     ensure => present,
-    require => Exec['apt-get-update'],
+    require => Exec['local-apt-update'],
   }
 
   service { 'php5-fpm':
